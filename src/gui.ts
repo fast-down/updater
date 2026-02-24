5015780e763f072775a57bf5df1d31ed168dd43e
share121 <me@s121.top>
2026-02-24 17:14:15 +0800

    fix: 修复 gui 链接不正确的问题

--- src/gui.ts
+++ src/gui.ts
@@ -1,35 +1,44 @@
 import { Hono } from "hono";
 import * as cheerio from "cheerio";
 
-const latestJsonUrl =
-  "https://github.com/fast-down/gui/releases/latest/download/latest.json";
-
 export const guiApp = new Hono()
   .get("/latest", async (c) => {
-    const res = <Latest>await fetch(latestJsonUrl).then((e) => e.json());
-    for (const i in res.platforms) {
-      res.platforms[i].url = res.platforms[i].url.replace(
-        "https://github.com/fast-down/gui/releases/download/",
-        "https://fast-down-update.s121.top/gui/raw/download/",
-      );
-    }
+    const version = await getLatestVersion();
     const infoResp = await fetch(
-      `https://github.com/fast-down/gui/releases/tag/fast-down-v${res.version}`,
+      `https://github.com/fast-down/gui/releases/expanded_assets/v${version}`,
     );
     const html = await infoResp.text();
     const $ = cheerio.load(html, {
       baseURI: infoResp.url,
     });
-    $(".markdown-body a").prop("target", "_blank");
-    res.notes = $(".markdown-body").html() || "修复了一些已知问题";
-    return c.json(res);
+    const assets = $("ul>li")
+      .map((_, el) => {
+        const a = $(el).find("a");
+        return {
+          name: a.text().trim(),
+          url: a.prop("href")!,
+        };
+      })
+      .toArray()
+      .filter((item) => !item.name.includes("Source code"))
+      .map((item) => {
+        const parts = item.url.split("/").at(-1)!.split(".")[0].split("-");
+        return {
+          platform: parts[2],
+          arch: parts[3],
+        };
+      });
+    return c.json({
+      version,
+      assets,
+    });
   })
   .get("/download/:version/:platform/:arch", async (c) => {
     const version = c.req.param("version");
     const platform = c.req.param("platform");
     const arch = c.req.param("arch");
     const { tag, filename } = await genReleaseUrl(version, platform, arch);
-    const url = `https://github.com/fast-down/gui/releases/download/${tag}/${filename}`;
+    const url = `https://github.com/fast-down/gui/releases/download/v${tag}/${filename}`;
     return c.redirect(url);
   });
 
@@ -40,19 +49,16 @@ export const guiApp = new Hono()
  * @param arch 架构，`64bit`、`32bit`、`arm64`
  */
 async function genReleaseUrl(version: string, platform: string, arch: string) {
-  if (version === "latest") {
-    const latest = <Latest>await fetch(latestJsonUrl).then((e) => e.json());
-    version = latest.version;
-  }
-  const filenameMap: Record<string, string | undefined> = {
-    "linux-64bit": `fast-down-gui_${version}_amd64.AppImage`,
-    "linux-arm64": `fast-down-gui_${version}_aarch64.AppImage`,
-    "macos-64bit": "fast-down-gui_x64.app.tar.gz",
-    "macos-arm64": "fast-down-gui_aarch64.app.tar.gz",
-    "windows-64bit": `fast-down-gui_${version}_x64-setup.exe`,
-    "windows-32bit": `fast-down-gui_${version}_x86-setup.exe`,
+  if (version === "latest") version = await getLatestVersion();
+  return {
+    tag: version,
+    filename: `fast-down-${platform}-${arch}${platform === "windows" ? ".exe" : ""}`,
   };
-  const filename = filenameMap[`${platform}-${arch}`];
-  if (!filename) throw new Error("Unsupported platform or arch");
-  return { tag: `fast-down-v${version}`, filename };
+}
+
+async function getLatestVersion() {
+  const releasesResp = await fetch(
+    "https://github.com/fast-down/gui/releases/latest/",
+  );
+  return releasesResp.url.split("/").at(-1)!.split("v")[1];
 }
